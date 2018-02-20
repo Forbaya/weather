d056c7d0835eaf3a321b4b40685fc4fdc8da86ff
Ville <vilunen@gmail.com>
2018-02-21 00:12:06 +0200

    Fixed checkstyle

--- src/main/java/com/forbaya/weather/domain/ObservationPoint.java
+++ src/main/java/com/forbaya/weather/domain/ObservationPoint.java
@@ -5,8 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 import java.util.List;
-import javax.persistence.*;
+
 
 @Entity
 @AllArgsConstructor
