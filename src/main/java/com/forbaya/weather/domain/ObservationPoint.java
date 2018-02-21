bc6d7e0a16d24d17f397e6936102fcdc43ee3c2f
Ville <vilunen@gmail.com>
2018-02-21 08:30:15 +0200

    Actually fixed checkstyle

--- src/main/java/com/forbaya/weather/domain/ObservationPoint.java
+++ src/main/java/com/forbaya/weather/domain/ObservationPoint.java
@@ -5,10 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
-import java.util.List;
 
 
 @Entity
